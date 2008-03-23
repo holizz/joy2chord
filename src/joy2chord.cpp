f12bbfbf4bdaf77c4b7c09e28db6be89234b1648
wirelessdreamer <wirelessdreamer@ed307cc0-c148-0410-9fe5-eebad300f2d8>
2008-03-23 20:25:49 +0000

    cleaned up vaiable naming for modes and updated keymaps to have some common codes between each mode
    git-svn-id: https://joy2chord.svn.sourceforge.net/svnroot/joy2chord@5 ed307cc0-c148-0410-9fe5-eebad300f2d8

--- src/joy2chord.cpp
+++ src/joy2chord.cpp
@@ -50,13 +50,8 @@ int joystick_no = 0;
 int axes;
 int joy_fd;
 int buttons;
-int verbose = 0;
+int verbose = 1;
 int maxbad = 40000;
-
-typedef struct {
-        int controller;
-        int last_value;
-}  val;
 
 int open_joystick()
 {
@@ -205,11 +200,6 @@ void send_key_up(__u16 code){
 void main_loop()
 {
         struct js_event js;
-        //double val_d;
-        //int val_i;
-        //int i;
-        //val *values;
-        //int data[256][256];
 
 	int joy_b1 = 4;
 	int joy_b2 = 6;
@@ -221,14 +211,18 @@ void main_loop()
 	int button_state[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	int send_code[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 	int button_code = 0;
-	int mode = 0;
+	int mode = 1;
+	int mode3count = 0;
+	int doubleclick = 0;
+
 	int mode1_code = 31; // this key combination (thumb 1 + 4 finger buttons changes to mode 1
 	int mode2_code = 47; // 47 this key combination ( thumb 2 + 4 finger buttons changes to mode 2
 	int mode3_code = 63; // 63 this key combination ( thumb 1 & 2 + 4 finger buttons changes to mode 3
 
-	int total_modes = 2; // 0 is included in this, so 2 is really 3
+	int total_modes = 3; 
 	__u16 modes[total_modes][64];
-
+	__u16 lastkey = KEY_RESERVED; // initilize the last key to nothing
+	__u16 thiskey = KEY_RESERVED; // for tracking the current key
 
 	int total_mods = 4; // if this is changed the if statement where codes are sent needs altered also
 	int norelease = 0;
@@ -246,109 +240,47 @@ void main_loop()
 	int shift = 0;
 
 	//Mappings from /usr/include/linux/input.h	
-	modes[0][1] = KEY_A;
-        modes[0][2] = KEY_B;
-        modes[0][3] = KEY_C;
-        modes[0][4] = KEY_D;
-        modes[0][5] = KEY_E;
-        modes[0][6] = KEY_F;
-        modes[0][7] = KEY_G;
-        modes[0][8] = KEY_H;
-        modes[0][9] = KEY_I;
-        modes[0][10] = KEY_J;
-        modes[0][11] = KEY_K;
-        modes[0][12] = KEY_L;
-        modes[0][13] = KEY_M;
-        modes[0][14] = KEY_N;
-        modes[0][15] = KEY_O;
-        modes[0][16] = KEY_P;
-        modes[0][17] = KEY_Q;
-        modes[0][18] = KEY_R;
-        modes[0][19] = KEY_S;
-        modes[0][20] = KEY_T;
-        modes[0][21] = KEY_U;
-        modes[0][22] = KEY_V;
-        modes[0][23] = KEY_W;
-        modes[0][24] = KEY_X;
-        modes[0][25] = KEY_Y;
-        modes[0][26] = KEY_Z;
-        modes[0][27] = KEY_RESERVED;	// set unused keys to KEY_RESERVED
-        modes[0][28] = KEY_LEFTMETA;
-        modes[0][29] = KEY_LEFTCTRL;
-        modes[0][30] = KEY_LEFTALT;
-	modes[0][31] = KEY_KBDILLUMTOGGLE; // using this for mode one
-	modes[0][32] = KEY_ENTER;
-	modes[0][33] = KEY_SPACE;
-	modes[0][34] = KEY_TAB;
-	modes[0][35] = KEY_BACKSPACE;
-	modes[0][36] = KEY_LEFTSHIFT;
-	modes[0][37] = KEY_RESERVED;
-	modes[0][38] = KEY_RESERVED;
-	modes[0][39] = KEY_RESERVED;
-	modes[0][40] = KEY_ESC;
-	modes[0][41] = KEY_PAGEUP;
-	modes[0][42] = KEY_PAGEDOWN;
-	modes[0][43] = KEY_HOME;
-	modes[0][44] = KEY_END;
-	modes[0][45] = KEY_INSERT;
-	modes[0][46] = KEY_DELETE;
-	modes[0][47] = KEY_KBDILLUMDOWN; //using this for mode two
-	modes[0][48] = KEY_RESERVED;
-	modes[0][49] = KEY_RESERVED;
-	modes[0][50] = KEY_RESERVED;
-	modes[0][51] = KEY_RESERVED;
-	modes[0][52] = KEY_RESERVED;
-	modes[0][53] = KEY_RESERVED;
-	modes[0][54] = KEY_RESERVED;
-	modes[0][55] = KEY_RESERVED;
-	modes[0][56] = KEY_RESERVED;
-	modes[0][57] = KEY_RESERVED;
-	modes[0][58] = KEY_RESERVED;
-	modes[0][59] = KEY_RESERVED;
-	modes[0][60] = KEY_RESERVED;
-	modes[0][61] = KEY_RESERVED;
-	modes[0][62] = KEY_RESERVED;
-	modes[0][63] = KEY_KBDILLUMUP; //using this for mode two
+	// position 0 never gets touched
 	
-	modes[1][1] = KEY_1;
-        modes[1][2] = KEY_2;
-        modes[1][3] = KEY_3;
-        modes[1][4] = KEY_4;
-        modes[1][5] = KEY_5;
-        modes[1][6] = KEY_6;
-        modes[1][7] = KEY_7;
-        modes[1][8] = KEY_8;
-        modes[1][9] = KEY_9;
-        modes[1][10] = KEY_0;
-        modes[1][11] = KEY_RESERVED;
-        modes[1][12] = KEY_RESERVED;
-        modes[1][13] = KEY_RESERVED;
-        modes[1][14] = KEY_RESERVED;
-        modes[1][15] = KEY_RESERVED;
-        modes[1][16] = KEY_RESERVED;
-        modes[1][17] = KEY_RESERVED;
-        modes[1][18] = KEY_RESERVED;
-        modes[1][19] = KEY_RESERVED;
-        modes[1][20] = KEY_RESERVED;
-        modes[1][21] = KEY_LEFTBRACE;
-        modes[1][22] = KEY_RIGHTBRACE;
-        modes[1][23] = KEY_RESERVED;
-        modes[1][24] = KEY_RESERVED;
-        modes[1][25] = KEY_RESERVED;
-        modes[1][26] = KEY_RESERVED;
-        modes[1][27] = KEY_RESERVED;	// set unused keys to KEY_RESERVED
-        modes[1][28] = KEY_RESERVED;
-        modes[1][29] = KEY_RESERVED;
-        modes[1][30] = KEY_RESERVED;
+	modes[1][1] = KEY_A;
+        modes[1][2] = KEY_B;
+        modes[1][3] = KEY_C;
+        modes[1][4] = KEY_D;
+        modes[1][5] = KEY_E;
+        modes[1][6] = KEY_F;
+        modes[1][7] = KEY_G;
+        modes[1][8] = KEY_H;
+        modes[1][9] = KEY_I;
+        modes[1][10] = KEY_J;
+        modes[1][11] = KEY_K;
+        modes[1][12] = KEY_L;
+        modes[1][13] = KEY_M;
+        modes[1][14] = KEY_N;
+        modes[1][15] = KEY_O;
+        modes[1][16] = KEY_P;
+        modes[1][17] = KEY_Q;
+        modes[1][18] = KEY_R;
+        modes[1][19] = KEY_S;
+        modes[1][20] = KEY_T;
+        modes[1][21] = KEY_U;
+        modes[1][22] = KEY_V;
+        modes[1][23] = KEY_W;
+        modes[1][24] = KEY_X;
+        modes[1][25] = KEY_Y;
+        modes[1][26] = KEY_Z;
+        modes[1][27] = KEY_LEFTSHIFT;
+	modes[1][28] = KEY_LEFTMETA;
+        modes[1][29] = KEY_LEFTCTRL;
+        modes[1][30] = KEY_LEFTALT;
 	modes[1][31] = KEY_KBDILLUMTOGGLE; // using this for mode one
 	modes[1][32] = KEY_ENTER;
 	modes[1][33] = KEY_SPACE;
 	modes[1][34] = KEY_TAB;
 	modes[1][35] = KEY_BACKSPACE;
-	modes[1][36] = KEY_LEFTSHIFT;
+	modes[1][36] = KEY_RESERVED;
 	modes[1][37] = KEY_RESERVED;
-	modes[1][38] = KEY_RESERVED;
-	modes[1][39] = KEY_RESERVED;
+	modes[1][38] = KEY_BRIGHTNESSDOWN; // binding for ctrl+alt+right
+	modes[1][39] = KEY_BRIGHTNESSUP; // binding for ctrl+alt+left
 	modes[1][40] = KEY_ESC;
 	modes[1][41] = KEY_PAGEUP;
 	modes[1][42] = KEY_PAGEDOWN;
@@ -374,52 +306,52 @@ void main_loop()
 	modes[1][62] = KEY_RESERVED;
 	modes[1][63] = KEY_KBDILLUMUP; //using this for mode two
 	
-	modes[2][1] = KEY_F1;
-        modes[2][2] = KEY_F2;
-        modes[2][3] = KEY_F3;
-        modes[2][4] = KEY_F4;
-        modes[2][5] = KEY_F5;
-        modes[2][6] = KEY_F6;
-        modes[2][7] = KEY_F7;
-        modes[2][8] = KEY_F8;
-        modes[2][9] = KEY_F9;
-        modes[2][10] = KEY_F10;
-        modes[2][11] = KEY_F11;
-        modes[2][12] = KEY_F12;
+	modes[2][1] = KEY_1;
+        modes[2][2] = KEY_2;
+        modes[2][3] = KEY_3;
+        modes[2][4] = KEY_4;
+        modes[2][5] = KEY_5;
+        modes[2][6] = KEY_6;
+        modes[2][7] = KEY_7;
+        modes[2][8] = KEY_8;
+        modes[2][9] = KEY_9;
+        modes[2][10] = KEY_0;
+        modes[2][11] = KEY_RESERVED;
+        modes[2][12] = KEY_RESERVED;
         modes[2][13] = KEY_RESERVED;
         modes[2][14] = KEY_RESERVED;
         modes[2][15] = KEY_RESERVED;
         modes[2][16] = KEY_RESERVED;
-        modes[2][17] = KEY_UP;
-        modes[2][18] = KEY_LEFT;
+        modes[2][17] = KEY_RESERVED;
+        modes[2][18] = KEY_RESERVED;
         modes[2][19] = KEY_RESERVED;
-        modes[2][20] = KEY_RIGHT;
-        modes[2][21] = KEY_RESERVED;
-        modes[2][22] = KEY_RESERVED;
+        modes[2][20] = KEY_RESERVED;
+        modes[2][21] = KEY_LEFTBRACE;
+        modes[2][22] = KEY_RIGHTBRACE;
         modes[2][23] = KEY_RESERVED;
-        modes[2][24] = KEY_DOWN;
+        modes[2][24] = KEY_RESERVED;
         modes[2][25] = KEY_RESERVED;
         modes[2][26] = KEY_RESERVED;
-        modes[2][27] = KEY_RESERVED;	// set unused keys to KEY_RESERVED
-        modes[2][28] = KEY_RESERVED;
-        modes[2][29] = KEY_RESERVED;
-        modes[2][30] = KEY_RESERVED;
+        modes[2][27] = KEY_LEFTSHIFT;	
+	modes[2][28] = KEY_LEFTMETA;
+        modes[2][29] = KEY_LEFTCTRL;
+        modes[2][30] = KEY_LEFTALT;
 	modes[2][31] = KEY_KBDILLUMTOGGLE; // using this for mode one
-	modes[2][32] = KEY_SYSRQ;
-	modes[2][33] = KEY_BREAK;
-	modes[2][34] = KEY_PAUSE;
-	modes[2][35] = KEY_CAPSLOCK;
-	modes[2][36] = KEY_NUMLOCK;
-	modes[2][37] = KEY_SCROLLLOCK;
+	modes[2][32] = KEY_ENTER;
+	modes[2][33] = KEY_SPACE;
+	modes[2][34] = KEY_TAB;
+	modes[2][35] = KEY_BACKSPACE;
+	modes[2][36] = KEY_RESERVED;
+	modes[2][37] = KEY_RESERVED;
 	modes[2][38] = KEY_RESERVED;
 	modes[2][39] = KEY_RESERVED;
-	modes[2][40] = KEY_RESERVED;
+	modes[2][40] = KEY_ESC;
 	modes[2][41] = KEY_RESERVED;
 	modes[2][42] = KEY_RESERVED;
-	modes[2][43] = KEY_RESERVED;
-	modes[2][44] = KEY_RESERVED;
-	modes[2][45] = KEY_RESERVED;
-	modes[2][46] = KEY_RESERVED;
+	modes[2][43] = KEY_HOME;
+	modes[2][44] = KEY_END;
+	modes[2][45] = KEY_INSERT;
+	modes[2][46] = KEY_DELETE;
 	modes[2][47] = KEY_KBDILLUMDOWN; //using this for mode two
 	modes[2][48] = KEY_RESERVED;
 	modes[2][49] = KEY_RESERVED;
@@ -437,6 +369,70 @@ void main_loop()
 	modes[2][61] = KEY_RESERVED;
 	modes[2][62] = KEY_RESERVED;
 	modes[2][63] = KEY_KBDILLUMUP; //using this for mode two
+
+	modes[3][1] = KEY_UP;
+        modes[3][2] = KEY_LEFT;
+        modes[3][3] = KEY_PAGEUP;
+        modes[3][4] = KEY_RIGHT;
+        modes[3][5] = KEY_RESERVED;
+        modes[3][6] = KEY_RESERVED;
+        modes[3][7] = KEY_RESERVED;
+        modes[3][8] = KEY_DOWN;
+        modes[3][9] = KEY_RESERVED;
+        modes[3][10] = KEY_RESERVED;
+        modes[3][11] = KEY_RESERVED;
+        modes[3][12] = KEY_PAGEDOWN;
+        modes[3][13] = KEY_RESERVED;
+        modes[3][14] = KEY_RESERVED;
+        modes[3][15] = KEY_RESERVED;
+        modes[3][16] = KEY_RESERVED;
+        modes[3][17] = KEY_RESERVED;
+        modes[3][18] = KEY_RESERVED;
+        modes[3][19] = KEY_RESERVED;
+        modes[3][20] = KEY_RESERVED;
+        modes[3][21] = KEY_RESERVED;
+        modes[3][22] = KEY_RESERVED;
+        modes[3][23] = KEY_RESERVED;
+        modes[3][24] = KEY_RESERVED;
+        modes[3][25] = KEY_RESERVED;
+        modes[3][26] = KEY_RESERVED;
+        modes[3][27] = KEY_LEFTSHIFT;
+	modes[3][28] = KEY_LEFTMETA;
+        modes[3][29] = KEY_LEFTCTRL;
+        modes[3][30] = KEY_LEFTALT;
+	modes[3][31] = KEY_KBDILLUMTOGGLE; // using this for mode one
+	modes[3][32] = KEY_ENTER;
+	modes[3][33] = KEY_F1;
+	modes[3][34] = KEY_F2;
+	modes[3][35] = KEY_F3;
+	modes[3][36] = KEY_F4;
+	modes[3][37] = KEY_F5;
+	modes[3][38] = KEY_F6;
+	modes[3][39] = KEY_F7;
+	modes[3][40] = KEY_F8;
+	modes[3][41] = KEY_F9;
+	modes[3][42] = KEY_F10;
+	modes[3][43] = KEY_F11;
+	modes[3][44] = KEY_F12;
+	modes[3][45] = KEY_RESERVED;
+	modes[3][46] = KEY_RESERVED;
+	modes[3][47] = KEY_KBDILLUMDOWN; //using this for mode two
+	modes[3][48] = KEY_RESERVED;
+	modes[3][49] = KEY_RESERVED;
+	modes[3][50] = KEY_RESERVED;
+	modes[3][51] = KEY_RESERVED;
+	modes[3][52] = KEY_RESERVED;
+	modes[3][53] = KEY_RESERVED;
+	modes[3][54] = KEY_RESERVED;
+	modes[3][55] = KEY_RESERVED;
+	modes[3][56] = KEY_RESERVED;
+	modes[3][57] = KEY_SYSRQ;
+	modes[3][58] = KEY_BREAK;
+	modes[3][59] = KEY_PAUSE;
+	modes[3][60] = KEY_CAPSLOCK;
+	modes[3][61] = KEY_NUMLOCK;
+	modes[3][62] = KEY_SCROLLLOCK;
+	modes[3][63] = KEY_KBDILLUMUP; //using this for mode two
 	
 	while (1){
 		if (read(joy_fd, &js, sizeof(struct js_event)) != sizeof(struct js_event)) {
@@ -478,7 +474,7 @@ void main_loop()
 	                                if (js.number == joy_b2){
 	                                        button_state[1] = 0;
 	                                }
-	                                if (js.number == joy_b3){
+					if (js.number == joy_b3){
 	                                        button_state[2] = 0;
 	                                }
 	                                if (js.number == joy_b4){
@@ -491,6 +487,7 @@ void main_loop()
 	                                        button_state[5] = 0;
 	                                } 
 				}
+				thiskey = modes[mode][button_code];	
 	                                // this is the "key code" that is going to be sent
 				// sanity checker, to make sure no bad data get through
 				if (send_code[0] > maxbad){send_code[0] = 0;}
@@ -500,52 +497,57 @@ void main_loop()
 				if (send_code[4] > maxbad){send_code[4] = 0;}
 				if (send_code[5] > maxbad){send_code[5] = 0;}
 				
-				cout << "Before: 1:" << send_code[0] << " 2:"<< send_code[1] << " 3:" <<  send_code[2] << " 4:" << send_code[3] << " 5:" <<  send_code[4] << " 6:" << send_code[5] << " ctrl: " << ctrl << " alt: " << alt << " shift: " << shift << " meta: " << meta << endl;
+				if (verbose){
+					cout << "Before: 1:" << send_code[0] << " 2:"<< send_code[1] << " 3:" <<  send_code[2] << " 4:" << send_code[3] << " 5:" <<  send_code[4] << " 6:" << send_code[5] << " ctrl: " << ctrl << " alt: " << alt << " shift: " << shift << " meta: " << meta << endl;
+				}
 	                        button_code = (send_code[0] + (send_code[1] * 2) + (send_code[2] * 4) + (send_code[3] * 8) + (send_code[4] * 16) + (send_code[5] * 32));
 	                        if ((button_state[0] == 0) && (button_state[1] == 0) && (button_state[2] == 0) && (button_state[3] == 0) && (button_state[4] == 0) && (button_state[5] == 0) && (button_code != 0)){ // if all buttons are released then send the code and clear everything
 					if (button_code == mode1_code){
-						cout << "mode 0" <<endl;
-						mode = 0;
-					}
-					if (button_code == mode2_code){
 						cout << "mode 1" <<endl;
 						mode = 1;
 					}
-					if (button_code == mode3_code){
+					if (button_code == mode2_code){
 						cout << "mode 2" <<endl;
 						mode = 2;
 					}
+					if (button_code == mode3_code){
+						cout << "mode 3" <<endl;
+						mode = 3;
+					}
 					justpressed = 0;
-		 			if ((button_code != mode1_code) && (button_code != mode2_code) && (button_code != mode3_code) && (modes[mode][button_code] != KEY_RESERVED)){
-						cout << "Sending Down Code: " <<  button_code << endl;
-						send_key_down(modes[mode][button_code]);	
+		 			if ((button_code != mode1_code) && (button_code != mode2_code) && (button_code != mode3_code) && (thiskey != KEY_RESERVED)){
+						if (verbose){
+							cout << "Sending Down Code: " <<  button_code << endl;
+						}
+						send_key_down(thiskey);	
+						lastkey = thiskey;
 					}
 
-					if (modes[mode][button_code] == KEY_LEFTALT){ alt = 1; justpressed = 1;}
-					if (modes[mode][button_code] == KEY_LEFTSHIFT){ shift = 1; justpressed = 1; }
-					if (modes[mode][button_code] == KEY_LEFTCTRL){ ctrl = 1; justpressed = 1; }
-					if (modes[mode][button_code] == KEY_LEFTMETA){ meta = 1; justpressed = 1; }
+					if (thiskey == KEY_LEFTALT){ alt = 1; justpressed = 1;}
+					if (thiskey == KEY_LEFTSHIFT){ shift = 1; justpressed = 1; }
+					if (thiskey == KEY_LEFTCTRL){ ctrl = 1; justpressed = 1; }
+					if (thiskey == KEY_LEFTMETA){ meta = 1; justpressed = 1; }
 				
-					if ((button_code != mode1_code) && (button_code != mode2_code) && (button_code != mode3_code) && (modes[mode][button_code] != KEY_RESERVED)){
-						if (justpressed == 0){
+					if ((button_code != mode1_code) && (button_code != mode2_code) && (button_code != mode3_code) && (thiskey != KEY_RESERVED) && (justpressed == 0)){
+						if (verbose){
 							cout << "Sending Up Code: " <<  button_code << endl;
-							send_key_up(modes[mode][button_code]);	
-							if (alt == 1){ 
-								alt = 0;
-								send_key_up(KEY_LEFTALT);
-							}		
-							if (ctrl == 1){ 
-								ctrl = 0;
-								send_key_up(KEY_LEFTCTRL);
-							}
-							if (shift == 1){ 
-								shift = 0;
-								send_key_up(KEY_LEFTSHIFT);
-							}
-							if (meta == 1){ 
-								meta = 0;
-								send_key_up(KEY_LEFTMETA);
-							}
+						}
+						send_key_up(thiskey);	
+						if ((alt == 1) && (thiskey != KEY_TAB) && (thiskey != KEY_BACKSPACE)){ 
+							alt = 0;
+							send_key_up(KEY_LEFTALT);
+						}		
+						if (ctrl == 1){ 
+							ctrl = 0;
+							send_key_up(KEY_LEFTCTRL);
+						}
+						if (shift == 1){ 
+							shift = 0;
+							send_key_up(KEY_LEFTSHIFT);
+						}
+						if (meta == 1){ 
+							meta = 0;
+							send_key_up(KEY_LEFTMETA);
 						}
 					}	
 	
@@ -554,6 +556,7 @@ void main_loop()
 					send_code[clearl] = 0;
 				}
 	                        button_code = 0;
+				doubleclick = 0;
 				}
 	                        break;
 		}
